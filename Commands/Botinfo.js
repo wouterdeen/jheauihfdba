4869a64ce38656804fb90d3c10de99a27e3093fe
wouterdeen <wouterdeen@gmail.com>
2018-06-11 14:06:27 +0200

    Update Botinfo.js

--- Commands/Botinfo.js
+++ Commands/Botinfo.js
@@ -4,14 +4,15 @@ module.exports.run = async (bot, message, args) => {
   let boticon = bot.user.displayAvatarURL;
 
   let botEmbed = new Discord.RichEmbed()
-  .setDescription("Wat is deze bot wollah")
+  .setDescription("Beep beep boop.")
   .setColor("#ef8e07")
   .setThumbnail(boticon)
+  .setFooter("Ik heb geen moeder btw")
   .addField("Naam", bot.user.username)
   .addField("Geboren op", "2/6/2018 om 13:58")
   .addField("Gewicht bij geboorte", "2,73 megabytes")
   .addField("Papa", "Brickconomy (Wouter)")
-  .addField("Versie", "v0.2 (Beta)");
+  .addField("Leeftijd", "v0.2 (Beta)");
 
   return message.channel.send(botEmbed);
 }
